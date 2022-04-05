d9ff305d2ea14cc4802ad374b0fdcc4da4cd101a
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-04-05 11:35:00 -0400

    commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :clickbait
+
+  # private
+
+  def clickbait
+    unless title.include? "Won't Believe"
+      errors.add(:title, "Title must be sufficiantly clickbait-y")
+    end
+  end
+
 end
